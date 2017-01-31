1bbb43ec126a38f30911a9e50249509c512eee2b
Hugh Morrison <hmorri32@gmail.com>
2017-01-31 14:59:35 -0700

    es6 for bubble sort

--- scripts/bubble-sort.js
+++ scripts/bubble-sort.js
@@ -1,14 +1,15 @@
 
-var a = [33, 103, 3, 726, 200, 984, 198, 764, 9];
+const a = [33, 103, 3, 726, 200, 984, 198, 764, 9];
 
 function bubbleSort(items) {
-  var length = items.length;
-  for (var i = 0; i < length; i++) { //Number of passes
-    for (var j = 0; j < (length - i - 1); j++) {
+  const length = items.length;
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length; j++) {
       if(items[j] > items[j + 1]) {
-        var tmp = items[j];  //Temporary variable to hold the current number
-        items[j] = items[j + 1]; //Replace current number with adjacent number
-        items[j + 1] = tmp; //Replace adjacent number with current number
+        const tmp = items[j];
+        items[j] = items[j + 1];
+        items[j + 1] = tmp;
+        console.log(items)
       }
     }
   }
@@ -19,11 +20,11 @@ console.log(a);
 
 
 //  Take two.
-var bubbleSort = function(array) {
-  for (var i = 0; i < array.length; i++){
-    for(var j = 0; j < array.length; j++){
+const bubbleSort = array => {
+  for (let i = 0; i < array.length; i++){
+    for(let j = 0; j < array.length; j++){
       if(array[j] > array[j + 1]){
-        var tank = array[j];
+        let tank = array[j];
         array[j] = array[j + 1]
         array[j + 1] = tank
         console.log(array)
@@ -31,32 +32,31 @@ var bubbleSort = function(array) {
     }
   }
   return array
-}
+};
 
 bubbleSort([4, 1, 5, 3, 2, 8, 25, 11, 100, 10])
 
 
 ///  Take three
 
-function swap(items, first, second){
-    var temp = items[first];
-    items[first] = items[second];
-    items[second] = temp;
+function swapStuff(items, first, second){
+  let temp = items[first];
+  items[first] = items[second];
+  items[second] = temp;
 }
 
 function bubbleSort(items){
-    var len = items.length,
-        i, j, stop;
-    for (i = 0; i < len; i++){
-        for (j = 0; j < len; j++){
-            if (items[j] > items[j + 1]){
-                swap(items, j, j + 1);
-                console.log(items)
-            }
-        }
+  let length = items.length;
+  let i;
+  let j;
+  for (i = 0; i < length; i++){
+    for (j = 0; j < length; j++){
+      if (items[j] > items[j + 1]){
+        swapStuff(items, j, j + 1);
+        console.log(items)
+      }
     }
-
-    return items;
+  }
 }
 
 bubbleSort([10,1,3,2,6,4,5,8,9,11])
