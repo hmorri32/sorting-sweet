7bd2c1612a94d23b200a7c531e0e99d1cd22f754
Hugh Morrison <hmorri32@gmail.com>
2017-01-31 15:44:05 -0700

    write tests for strings in bubbler

--- tests/bubble-sort-test.js
+++ tests/bubble-sort-test.js
@@ -2,6 +2,8 @@ import { expect } from 'chai';
 import bubbleSort3 from '../scripts/bubble-sort'
 
 describe('TDD with bubbles', () => {
+  console.log('suh')
+
   it('should return an array', () => {
     expect(Array.isArray(bubbleSort3([], 'hello'))).to.be.true
   })
@@ -10,12 +12,16 @@ describe('TDD with bubbles', () => {
     expect( () => bubbleSort3('yo')).to.throw('error')
   })
 
-  it('should bubble sort and return ordered array items', () => {
+  it('should bubble sort numbers and return and ordered array', () => {
     let bubbler = bubbleSort3([1, 4, 3, 2, 7])
     // console.log(bubbler)
     expect(bubbler).to.deep.equal([1, 2, 3, 4, 7])
   })
+
+  it('should also bubble sort strings and return an ordered array', () => {
+    let bubbler = bubbleSort3(["yo", "brother", "man", "shakas", "bru", "taquitos", "tranquilo", "tropical"])
+    // console.log(bubbler)
+    expect(bubbler).to.deep.equal(["brother", "bru", "man", "shakas", "taquitos", "tranquilo", "tropical", "yo"])
+  })
+
 })
-
-
-console.log('suh')
