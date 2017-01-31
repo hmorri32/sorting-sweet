1d14ae9e51cd7b7c04bc3519ac1f49aa257d719d
Hugh Morrison <hmorri32@gmail.com>
2017-01-31 15:13:02 -0700

    make bubble sort test file. make first test.

--- scripts/bubble-sort.js
+++ scripts/bubble-sort.js
@@ -1,22 +1,21 @@
 
 const a = [33, 103, 3, 726, 200, 984, 198, 764, 9];
 
-function bubbleSort1(items) {
-  const length = items.length;
+function bubbleSort1(array) {
+  const length = array.length;
   for (let i = 0; i < length; i++) {
     for (let j = 0; j < length; j++) {
-      if(items[j] > items[j + 1]) {
-        const tmp = items[j];
-        items[j] = items[j + 1];
-        items[j + 1] = tmp;
-        console.log(items)
+      if(array[j] > array[j + 1]) {
+        const holdingTank = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = holdingTank;
+        console.log(array)
       }
     }
   }
 }
 
-bubbleSort1(a);
-console.log(a);
+// bubbleSort1(a);
 
 
 //  Take two.
@@ -30,19 +29,18 @@ const bubbleSort2 = array => {
         console.log(array)
       }
     }
-  }
-  return array
+  } return array
 };
 
-bubbleSort2([4, 1, 5, 3, 2, 8, 25, 11, 100, 10])
+// bubbleSort2([4, 1, 5, 3, 2, 8, 25, 11, 100, 10])
 
 
 ///  Take three
 
 function swapStuff(items, first, second){
-  let temp = items[first];
+  let tank = items[first];
   items[first] = items[second];
-  items[second] = temp;
+  items[second] = tank;
 }
 
 function bubbleSort3(items){
@@ -56,9 +54,12 @@ function bubbleSort3(items){
         console.log(items)
       }
     }
-  }
+  } return items
 }
 
-bubbleSort3([10,1,3,2,6,4,5,8,9,11])
+// bubbleSort3([10,1,3,2,11,6,4,5,8,9,])
 
 //
+
+
+module.exports =  bubbleSort3
--- tests/bubble-sort-test.js
+++ tests/bubble-sort-test.js
@@ -1,1 +1,13 @@
+import { expect } from 'chai';
+import bubbleSort3 from '../scripts/bubble-sort'
+
+describe('bubble sort tests maaannnnn', () => {
+  it('should return an array', () => {
+    expect(Array.isArray(bubbleSort3([], 'hello'))).to.be.true
+  })
+  
+})
+
+
+
 console.log('suh')
