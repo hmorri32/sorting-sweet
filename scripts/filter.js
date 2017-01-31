5d26163ee7dae10277c1cc646c2f3f1e3a1455a3
Hugh Morrison <hmorri32@gmail.com>
2017-01-31 14:46:08 -0700

    new sorting sweet

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,5 +1,21 @@
-const filterMe = (array) => {
-  return array
+const validateString = function(string){
+  if( typeof string !== 'string'){
+    throw new Error('error')
+  };
+  if (!string){
+    throw new Error('error')
+  }
 }
+
+const filterMe = (array, string) => {
+  validateString(string);
+
+  let newArray = array.filter((value)=>{
+    return value !== string
+  })
+  return newArray
+}
+
+
 
 export default filterMe
