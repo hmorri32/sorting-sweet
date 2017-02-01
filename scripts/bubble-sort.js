683ff74e4e7f826d5afa19692f60eeb75dfaaddb
Hugh Morrison <hmorri32@gmail.com>
2017-02-01 15:45:06 -0700

    write pseudo code for all files

--- scripts/bubble-sort.js
+++ scripts/bubble-sort.js
@@ -1,11 +1,17 @@
 
 //  Take two.
+
+
 const bubbleSort = array => {
+  // Two for loops. dále.
   for (let i = 0; i < array.length; i++){
     for(let j = 0; j < array.length; j++){
       if(array[j] > array[j + 1]){
+        // define a temporary holding tank for your value.
         let tank = array[j];
+        // switch the values.
         array[j] = array[j + 1]
+        // switch em back.
         array[j + 1] = tank
         console.log(array)
       }
@@ -15,14 +21,16 @@ const bubbleSort = array => {
 
 
 ///  Take three
-
+// Make helper function for that swapswap
 function swapStuff(items, first, second){
+  // temporary variable. holding tank. hold the value while altering the other
   let tank = items[first];
   items[first] = items[second];
   items[second] = tank;
 }
 
 function bubbleSort3(array){
+  // If array is not an array chuck an error, m8.
   if(!Array.isArray(array)){
     throw new Error('error')
   }
--- scripts/insertion-sort.js
+++ scripts/insertion-sort.js
@@ -1,19 +1,27 @@
-function insertionSort(stuff) {
-  const length = stuff.length;
+// Imagine this shit as though you were playing gin rummie
+// And needed to sort your cards.
 
+function insertionSort(stuff) {
+  // Grab length of the stuff.
+  const length = stuff.length;
+  // If the length of the stuff is less than 1 than throw
   if(length < 1){
     throw new Error('error')
   }
-
+  // if the array is not actually an array, throw
   if(!Array.isArray(stuff)){
     throw new Error('error')
   }
-
+  // Loop through the array front to front
   for(let i = 1; i < length; i++) {
-    const tank = stuff[i];
+    // Define a temporary variable that holds value
+    let tank = stuff[i];
+    // Loop through the stuff from i - 1 (j)
     for(var j = (i - 1); j >= 0 && stuff[j] > tank; j--) {
+      // if the stuff at j + 1 is bigger than the stuff at J, switch em.
       stuff[j+1] = stuff[j];
     }
+    // Overwrite j+1 with tank value
     stuff[j + 1] = tank;
     console.log(stuff)
   }
