f46d20127d213b1a27ba5963d1adaebd31450292
Hugh Morrison <hmorri32@gmail.com>
2017-02-01 14:24:30 -0700

    delete superflous code in insertion test

--- tests/insertion-sort-test.js
+++ tests/insertion-sort-test.js
@@ -27,18 +27,4 @@ describe('TDD with insertion sort', () => {
 
     expect(insertDis).to.deep.equal(['a', 'b', 'c', 'd', 'g', 'i', 'm'])
   })
-
-  //
-  // it('should bubble sort numbers and return and ordered array', () => {
-  //   let bubbler = bubbleSort3([1, 4, 3, 2, 7])
-  //
-  //   expect(bubbler).to.deep.equal([1, 2, 3, 4, 7])
-  // })
-  //
-  // it('should also bubble sort strings and return an ordered array', () => {
-  //   let bubbler = bubbleSort3(["z", "d", "a", "f", "b", "x", "m", "j"])
-  //
-  //   expect(bubbler).to.deep.equal(["a", "b", "d", "f", "j", "m", "x", "z"])
-  // })
-
 })
