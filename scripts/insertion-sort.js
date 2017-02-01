35e092c4f8e20f56872ab236ac8ef024ca83ca88
Hugh Morrison <hmorri32@gmail.com>
2017-02-01 14:20:11 -0700

    es6 for insertion sort

--- scripts/insertion-sort.js
+++ scripts/insertion-sort.js
@@ -7,16 +7,17 @@ function insertionSort(stuff) {
     throw new Error('error')
   }
 
-  var length = stuff.length;
+  const length = stuff.length;
 
-  for(var i = 1; i < length; i++) {
-    var tank = stuff[i];
+  for(let i = 1; i < length; i++) {
+    const tank = stuff[i];
     for(var j = (i - 1); j >= 0 && stuff[j] > tank; j--) {
       stuff[j+1] = stuff[j];
     }
     stuff[j + 1] = tank;
     console.log(stuff)
-  } return stuff
+  }
+  return stuff
 }
 
 // insertionSort([9, 20, 33, 15, 4, 7, 16, 2]);
