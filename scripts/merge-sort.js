94569eb90bcb2fcf117bce799eaf60274a102a60
Hugh Morrison <hmorri32@gmail.com>
2017-02-01 14:42:54 -0700

    write bones for merge sort

---
+++ scripts/merge-sort.js
@@ -0,0 +1,18 @@
+function divideAndConquer(leftArray, rightArray){
+  var finished = [];
+
+
+}
+
+
+
+function mergeStuff(stuff){
+  const length = stuff.length;
+  if(length < 2) {
+    return stuff;
+  }
+  var halfStuff  = Math.floor(length/2),
+  var leftArray  = stuff.slice(0, halfStuff),
+  var rightArray = stuff.slice(halfStuff);
+
+}
